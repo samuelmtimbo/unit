52818afb766656c6c6467fefa937e74c178cfb21
Victor Elias <victorgelias@gmail.com>
2021-10-03 11:23:28 -0300

    Prototype Unith types

---
+++ ethereum/unith.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.4.20;
+
+library U {
+    enum DataType {
+        String,
+        Number,
+        Array,
+        Object
+    }
+
+    // this struct is insanely expensive
+    struct Data {
+        DataType type_;
+        string str;
+        uint128 number; // numbers are shifted <<64
+        // Data[] array; // recursive type error
+        // object:
+        string[] keys;
+        // Data[] values;
+    }
+}
+
+interface OutputHandler {
+    function take(U.Data memory result) external;
+}
+
+interface Unit {
+    function take(U.Data memory input, OutputHandler output) external;
+}
+
+interface UnitFactory {
+    function create() external view returns (Unit);
+}
+
+contract Mothership {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    mapping(string => UnitFactory) public units;
+
+    function register(string memory name, UnitFactory ufac) public {
+        require(
+            msg.sender == owner,
+            'Only owner can register units (for now).'
+        );
+        require(address(units[name]) == address(0), 'Unit already exists.');
+
+        units[name] = ufac;
+    }
+
+    function get(string memory name) public view returns (Unit) {
+        return units[name].create();
+    }
+}
