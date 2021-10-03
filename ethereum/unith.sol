602b1e7e97e85d7eb190c5c469d091d3561e3042
Victor Elias <victorgelias@gmail.com>
2021-10-03 12:10:13 -0300

    Add Data/Heap/Type helpers

--- ethereum/unith.sol
+++ ethereum/unith.sol
@@ -9,7 +9,7 @@ library U {
         Object
     }
 
-    // this struct is insanely expensive
+    // this struct is NOT ANYMORE insanely expensive
     struct Data {
         DataType type_;
         // location is polymorphic depending on type above:
@@ -22,17 +22,69 @@ library U {
 
     struct Heap {
         string[] strings;
-        uint128[] numbers; // numbers are shifted <<64
+        int128[] numbers; // numbers are shifted <<63
         Data[] objects;
+    }
+
+    function nitString(U.Heap memory heap, U.Data memory value)
+        internal
+        pure
+        returns (string memory)
+    {
+        assert(value.type_ == DataType.String);
+        return heap.strings[value.location[0]];
+    }
+
+    function nitNumber(U.Heap memory heap, U.Data memory value)
+        internal
+        pure
+        returns (int128)
+    {
+        assert(value.type_ == DataType.Number);
+        return heap.numbers[value.location[0]];
+    }
+
+    // now this one might be (expensive). consider implementing one that calls a function for each element instead.
+    function nitArray(U.Heap memory heap, U.Data memory value)
+        internal
+        pure
+        returns (Data[] memory array)
+    {
+        assert(value.type_ == DataType.Array);
+        array = new Data[](value.location.length);
+        for (uint32 i = 0; i < array.length; i++) {
+            array[i] = heap.objects[value.location[i]];
+        }
+        return array;
+    }
+
+    // same as above.
+    function nitObject(U.Heap memory heap, U.Data memory value)
+        internal
+        pure
+        returns (string[] memory keys, Data[] memory values)
+    {
+        assert(value.type_ == DataType.Object);
+        keys = new string[](value.location.length / 2);
+        values = new Data[](value.location.length / 2);
+        for (uint32 i = 0; i < keys.length; i++) {
+            keys[i] = heap.strings[value.location[2 * i]];
+            values[i] = heap.objects[value.location[2 * i + 1]];
+        }
+        return (keys, values);
     }
 }
 
 interface OutputHandler {
-    function take(U.Data memory result) external;
+    function take(U.Heap memory heap, U.Data memory result) external;
 }
 
 interface Unit {
-    function take(U.Data memory input, OutputHandler output) external;
+    function take(
+        U.Heap memory heap,
+        U.Data memory input,
+        OutputHandler output
+    ) external;
 }
 
 interface UnitFactory {
