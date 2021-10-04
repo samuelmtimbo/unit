8a92f7b66e4fefd42c1614cb9ca8f6cb9ad3769f
Victor Elias <victorgelias@gmail.com>
2021-10-03 22:14:51 -0300

    Make the heap a contract instead
    Makes things much cleaner. Also store heap in units storage.

--- ethereum/unith.sol
+++ ethereum/unith.sol
@@ -1,52 +1,48 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.4.20;
 
+contract Heap {
+    string[] strings;
+    int128[] numbers; // numbers are shifted <<63
+    U.Data[] objects;
+
+    function newString(string memory value) internal returns (uint32 addr) {
+        strings.push(value);
+        addr = uint32(strings.length - 1);
+    }
+
+    function getString(uint32 addr) internal view returns (string storage) {
+        return strings[addr];
+    }
+
+    function setString(uint32 addr, string memory value) internal {
+        strings[addr] = value;
+    }
+
+    function newNumber(int128 value) internal returns (uint32 addr) {
+        numbers.push(value);
+        addr = uint32(numbers.length - 1);
+    }
+
+    function getNumber(uint32 addr) internal view returns (int128) {
+        return numbers[addr];
+    }
+
+    function setNumber(uint32 addr, int128 value) internal {
+        numbers[addr] = value;
+    }
+
+    function newObject(U.Data memory value) internal returns (uint32 addr) {
+        objects.push(value);
+        addr = uint32(objects.length - 1);
+    }
+
+    function getObject(uint32 addr) internal view returns (U.Data storage) {
+        return objects[addr];
+    }
+}
+
 library U {
-    struct Heap {
-        string[] strings;
-        int128[] numbers; // numbers are shifted <<63
-        Data[] objects;
-    }
-
-    function expandStrings(U.Heap memory heap, uint32 extra)
-        internal
-        pure
-        returns (uint32 prevLen)
-    {
-        prevLen = uint32(heap.strings.length);
-        string[] memory expanded = new string[](prevLen + extra);
-        for (uint32 i = 0; i < prevLen; i++) {
-            expanded[i] = heap.strings[i];
-        }
-        heap.strings = expanded;
-    }
-
-    function expandNumbers(U.Heap memory heap, uint32 extra)
-        internal
-        pure
-        returns (uint32 prevLen)
-    {
-        prevLen = uint32(heap.numbers.length);
-        int128[] memory expanded = new int128[](prevLen + extra);
-        for (uint32 i = 0; i < prevLen; i++) {
-            expanded[i] = heap.numbers[i];
-        }
-        heap.numbers = expanded;
-    }
-
-    function expandObjects(U.Heap memory heap, uint32 extra)
-        internal
-        pure
-        returns (uint32 prevLen)
-    {
-        prevLen = uint32(heap.objects.length);
-        U.Data[] memory expanded = new U.Data[](prevLen + extra);
-        for (uint32 i = 0; i < prevLen; i++) {
-            expanded[i] = heap.objects[i];
-        }
-        heap.objects = expanded;
-    }
-
     enum DataType {
         String,
         Number,
@@ -65,50 +61,46 @@ library U {
         uint32[] location;
     }
 
-    function getString(U.Heap memory heap, U.Data memory value)
+    function asString(Heap heap, U.Data memory value)
         internal
         pure
-        returns (string memory)
+        returns (string storage)
     {
         require(value.type_ == DataType.String, 'Type must be string');
-        return heap.strings[value.location[0]];
+        return heap.getString(value.location[0]);
     }
 
-    function nitString(U.Heap memory heap, string memory value)
+    function nitString(Heap heap, string memory value)
         internal
         pure
         returns (U.Data memory)
     {
-        uint32 prevLen = expandStrings(heap, 1);
         uint32[] memory location = new uint32[](1);
-        heap.strings[prevLen] = value;
-        location[0] = prevLen;
-        return U.Data({ type_: DataType.String, location: location });
+        location[0] = heap.newString(value);
+        return U.Data(DataType.String, location);
     }
 
-    function getNumber(U.Heap memory heap, U.Data memory value)
+    function asNumber(Heap heap, U.Data memory value)
         internal
         pure
         returns (int128)
     {
         require(value.type_ == DataType.Number, 'Type must be number');
-        return heap.numbers[value.location[0]];
+        return heap.getNumber(value.location[0]);
     }
 
-    function nitNumber(U.Heap memory heap, int128 value)
+    function nitNumber(Heap heap, int128 value)
         internal
         pure
         returns (U.Data memory)
     {
-        uint32 prevLen = expandNumbers(heap, 1);
         uint32[] memory location = new uint32[](1);
-        heap.numbers[prevLen] = value;
-        location[0] = prevLen;
-        return U.Data({ type_: DataType.Number, location: location });
+        location[0] = heap.newNumber(value);
+        return U.Data(DataType.Number, location);
     }
 
     // now this one might be (expensive). consider implementing one that calls a function for each element instead.
-    function getArray(U.Heap memory heap, U.Data memory value)
+    function asArray(Heap heap, U.Data memory value)
         internal
         pure
         returns (Data[] memory array)
@@ -116,28 +108,24 @@ library U {
         require(value.type_ == DataType.Array, 'Type must be array');
         array = new Data[](value.location.length);
         for (uint32 i = 0; i < array.length; i++) {
-            array[i] = heap.objects[value.location[i]];
+            array[i] = heap.getObject(value.location[i]);
         }
-        return array;
     }
 
-    function nitArray(U.Heap memory heap, U.Data[] memory array)
+    function nitArray(Heap heap, U.Data[] memory array)
         internal
         pure
         returns (U.Data memory)
     {
-        uint32 prevLen = expandObjects(heap, uint32(array.length));
         uint32[] memory location = new uint32[](array.length);
         for (uint32 i = 0; i < array.length; i++) {
-            uint32 heapIdx = prevLen + i;
-            heap.objects[heapIdx] = array[i];
-            location[i] = heapIdx;
+            location[i] = heap.newObject(array[i]);
         }
-        return U.Data({ type_: DataType.Array, location: location });
+        return U.Data(DataType.Array, location);
     }
 
     // same as above.
-    function getObject(U.Heap memory heap, U.Data memory value)
+    function getObject(Heap heap, U.Data memory value)
         internal
         pure
         returns (string[] memory keys, Data[] memory values)
@@ -146,14 +134,14 @@ library U {
         keys = new string[](value.location.length / 2);
         values = new Data[](value.location.length / 2);
         for (uint32 i = 0; i < keys.length; i++) {
-            keys[i] = heap.strings[value.location[2 * i]];
-            values[i] = heap.objects[value.location[2 * i + 1]];
+            keys[i] = heap.getString(value.location[2 * i]);
+            values[i] = heap.getObject(value.location[2 * i + 1]);
         }
         return (keys, values);
     }
 
-    function niObject(
-        U.Heap memory heap,
+    function nitObject(
+        Heap heap,
         string[] memory keys,
         U.Data[] memory values
     ) internal pure returns (U.Data memory) {
@@ -161,43 +149,31 @@ library U {
             keys.length == values.length,
             'Object must have same number of keys and values'
         );
-        uint32 prevStrsLen = expandStrings(heap, uint32(keys.length));
-        uint32 prevObjsLen = expandObjects(heap, uint32(values.length));
         uint32[] memory location = new uint32[](2 * keys.length);
         for (uint32 i = 0; i < keys.length; i++) {
-            uint32 keyIdx = prevStrsLen + i;
-            uint32 valueIdx = prevObjsLen + i;
-            heap.strings[keyIdx] = keys[i];
-            heap.objects[valueIdx] = values[i];
-            location[2 * i] = keyIdx;
-            location[2 * i + 1] = valueIdx;
+            location[2 * i] = heap.newString(keys[i]);
+            location[2 * i + 1] = heap.newObject(values[i]);
         }
-        return U.Data({ type_: DataType.Object, location: location });
+        return U.Data(DataType.Object, location);
     }
 }
 
 interface OutputHandler {
-    function take(U.Heap memory heap, U.Data memory result) external;
+    function take(U.Data memory result) external;
 }
 
 interface Unit {
-    function take(
-        U.Heap memory heap,
-        U.Data memory input,
-        OutputHandler output
-    ) external;
+    function take(U.Data memory input, OutputHandler output) external;
 }
 
 interface UnitFactory {
-    function create() external returns (Unit);
+    function create(Heap heap) external returns (Unit);
 }
 
 contract Add is Unit {
-    function take(
-        U.Heap memory heap,
-        U.Data memory input,
-        OutputHandler done
-    ) external {
+    Heap heap;
+
+    function take(U.Data memory input, OutputHandler done) external {
         U.Data[] memory inputs = U.getArray(heap, input);
         int128 a = U.getNumber(heap, inputs[0]);
         int128 b = U.getNumber(heap, inputs[1]);
@@ -208,8 +184,10 @@ contract Add is Unit {
 }
 
 contract AddFactory is UnitFactory {
-    function create() external returns (Unit) {
-        return new Add();
+    function create(Heap heap) external returns (Unit) {
+        Add add = new Add();
+        add.heap = heap;
+        return add;
     }
 }
 
