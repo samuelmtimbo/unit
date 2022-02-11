1b8c9e23e7d80b86c5c8aba08259aae9688b29a1
Samuel Timbó <samuelmtimbo@gmail.com>
2022-02-11 18:34:15 -0300

    recover extension code

---
+++ src/client/extension/src/background.ts
@@ -0,0 +1,18 @@
+const EXTENSION_ID = 'hcecpkhphpabohdkkpnenodimblpnglc'
+
+// @ts-ignore
+chrome.runtime.onConnect.addListener(port => {})
+// @ts-ignore
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  // console.log(message)
+  if (sender.id === EXTENSION_ID) {
+    const { type, method, data } = message
+    // @ts-ignore
+    chrome[type][method](data, (result) => {
+      sendResponse(result)
+    })
+    return true
+  } else {
+    return false
+  }
+})
---
+++ src/client/extension/src/popup.ts
@@ -0,0 +1,4 @@
+// @ts-ignore
+chrome.storage.sync.get('color', function (data) { })
+
+export default null
