3d0c49b27416e93a9edec13556a4f11c0c43d7bd
Samuel MT <samuelmtimbo@gmail.com>
2023-12-18 19:30:19 -0300

    remove old unused files

--- src/client/platform/web/createIframe.ts
+++
@@ -1,9 +0,0 @@
-export default function createIframe(): HTMLIFrameElement {
-  const iframe = document.createElement('iframe')
-
-  iframe.style.border = 'none'
-  iframe.style.width = '100%'
-  iframe.style.height = '100%'
-
-  return iframe
-}
