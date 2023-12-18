203e139f65371d152e856ed13db22ec5cc728715
Samuel MT <samuelmtimbo@gmail.com>
2023-12-18 19:30:19 -0300

    remove old unused files

--- src/client/setupCanvas.ts
+++
@@ -1,9 +0,0 @@
-export function setupCanvas(canvas: HTMLCanvasElement) {
-  const canvas_ctx = canvas.getContext('2d')
-  const dpr = window.devicePixelRatio || 1
-  const rect = canvas.getBoundingClientRect()
-  canvas.width = rect.width * dpr
-  canvas.height = rect.height * dpr
-  canvas_ctx.scale(dpr, dpr)
-  canvas.style.transform = `scale(${1 / dpr})`
-}
