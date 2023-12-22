c6cb16d335534ba9e05e458ee2c62965ab53060a
Karan Handa <karan.handa.fox@gmail.com>
2023-12-22 21:15:06 +0530

    fix invalid object literal blue click infinite loop

--- src/types/parser/randomValue.ts
+++ src/types/parser/randomValue.ts
@@ -79,21 +79,12 @@ export function randomTreeOfObjectLiteral(
 ): TreeNode {
   const children = []
 
-  if (typeTree.children.length === 0) {
-    let key_tree = getTree("'foo'")
-    let value_tree = getTree("'foo'")
-
-    let sample_key_value: TreeNode = {
-      value: `${key_tree.value}:${value_tree.value}`,
-      children: [key_tree, value_tree],
-      type: TreeNodeType.KeyValue,
-    }
-
-    children.push(sample_key_value)
-  }
-
   for (const type_key_value of typeTree.children) {
     const [type_key_tree, type_value_tree] = type_key_value.children
+
+    if (type_value_tree.type === TreeNodeType.Invalid) {
+      continue;
+    }
 
     const { value: key } = type_key_tree
 
@@ -120,6 +111,20 @@ export function randomTreeOfObjectLiteral(
       children.push(key_value_tree)
     }
   }
+
+  if (children.length === 0) {
+    let key_tree = getTree("'foo'")
+    let value_tree = getTree("'foo'")
+
+    let sample_key_value: TreeNode = {
+      value: `${key_tree.value}:${value_tree.value}`,
+      children: [key_tree, value_tree],
+      type: TreeNodeType.KeyValue,
+    }
+
+    children.push(sample_key_value)
+  }
+
   return {
     value: `{${children.map(propValue).join(',')}}`,
     children,
