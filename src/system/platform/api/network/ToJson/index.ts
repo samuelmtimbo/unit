349a1abf0ad0121a9b776427de66fa4b3232173b
Samuel MT <samuelmtimbo@gmail.com>
2024-01-01 18:31:28 -0300

    new unit http response `to json`

---
+++ src/system/platform/api/network/ToJson/index.ts
@@ -0,0 +1,35 @@
+import { $ } from '@_unit/unit/lib/Class/$'
+import { Functional } from '../../../../../Class/Functional'
+import { Done } from '../../../../../Class/Functional/Done'
+import { System } from '../../../../../system'
+import { RES } from '../../../../../types/interface/RES'
+import { ID_TO_JSON } from '../../../../_ids'
+
+export type I = {
+  res: RES & $
+  any: any
+}
+
+export type O = {
+  json: any
+}
+
+export default class ToJson extends Functional<I, O> {
+  constructor(system: System) {
+    super(
+      {
+        i: ['res', 'any'],
+        o: ['json'],
+      },
+      {},
+      system,
+      ID_TO_JSON
+    )
+  }
+
+  async f({ res }: I, done: Done<O>) {
+    const json = await res.toJson()
+
+    done({ json })
+  }
+}
--- src/wrap/Response.ts
+++ src/wrap/Response.ts
@@ -5,6 +5,10 @@ import { RES } from '../types/interface/RES'
 export function wrapResponse(response: Response, system: System): RES & $ {
   const $response = new (class Response_ extends $ implements RES {
     __: string[] = ['RES']
+
+    toJson(): Promise<any> {
+      return response.json()
+    }
   })(system)
 
   return $response
