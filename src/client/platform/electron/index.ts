ff1f685643894be709cd9c7224ab9e0e5301171a
Samuel MT <samuelmtimbo@gmail.com>
2024-01-04 14:27:33 -0300

    add electron

---
+++ src/client/platform/electron/index.ts
@@ -0,0 +1,20 @@
+import { app, BrowserWindow } from 'electron'
+
+import { serve } from '../../../server/serve'
+
+const PORT = 4224
+
+const server = serve({ port: PORT })
+
+const createWindow = () => {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+
+  window.loadURL(`http://localhost:${PORT}`)
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
