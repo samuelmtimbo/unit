68d83068e4588b5afea6e584bd314e22d0e6e2bc
Samuel MT <samuelmtimbo@gmail.com>
2024-04-30 16:52:51 -0300

    `radio field` and `fieldset`

---
+++ src/system/platform/component/value/RadioField/Component.ts
@@ -0,0 +1,24 @@
+import { Field } from '../../../../../client/field'
+import { System } from '../../../../../system'
+import { Dict } from '../../../../../types/Dict'
+
+export interface Props {
+  className?: string
+  style?: Dict<any>
+  value?: string
+  disabled?: boolean
+}
+
+export const DEFAULT_STYLE = {}
+
+export default class RadioField extends Field<HTMLInputElement, Props> {
+  constructor($props: Props, $system: System) {
+    super($props, $system, $system.api.document.createElement('input'), {
+      valueKey: 'value',
+      defaultStyle: DEFAULT_STYLE,
+      defaultValue: '',
+    })
+
+    this.$element.type = 'radio'
+  }
+}
