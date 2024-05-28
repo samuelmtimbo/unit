10750e86f6853160638c49315b758fad7f0751fc
Samuel MT <samuelmtimbo@gmail.com>
2024-05-27 22:40:01 -0300

    remove example `range rec` from core (use for test)

--- src/test/system/core/RangeRec.ts
+++ src/test/system/core/RangeRec.ts
@@ -6,9 +6,11 @@ import {
 } from '../../../debug'
 import { fromSpec } from '../../../spec/fromSpec'
 import _specs from '../../../system/_specs'
+import { GraphSpec } from '../../../types/GraphSpec'
 import { system } from '../../util/system'
 
-const spec = require('../../../system/core/loop/RangeRec/spec.json')
+const spec = require('./RangeRec.json') as GraphSpec
+
 const Range = fromSpec<{ any: any }, { bit: number }>(spec, _specs, {})
 
 const range = new Range(system)
