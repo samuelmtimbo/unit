9f83abe63f4a2983e4eee34946033ea496536e02
Samuel MT <samuelmtimbo@gmail.com>
2024-08-16 21:01:51 -0300

    `graph` elevation unit

---
+++ src/system/f/meta/Graph/index.ts
@@ -0,0 +1,52 @@
+import { $ } from '../../../../Class/$'
+import { Functional } from '../../../../Class/Functional'
+import { Done } from '../../../../Class/Functional/Done'
+import { Graph } from '../../../../Class/Graph'
+import { System } from '../../../../system'
+import { G } from '../../../../types/interface/G'
+import { ID_GRAPH } from '../../../_ids'
+
+export interface I<T> {
+  unit: $
+}
+
+export interface O<T> {
+  graph: G
+}
+
+export default class Graph0<T> extends Functional<I<T>, O<T>> {
+  constructor(system: System) {
+    super(
+      {
+        i: ['unit'],
+        o: ['graph'],
+      },
+      {
+        input: {
+          unit: {
+            ref: true,
+          },
+        },
+        output: {
+          graph: {
+            ref: true,
+          },
+        },
+      },
+      system,
+      ID_GRAPH
+    )
+  }
+
+  f({ unit }: I<T>, done: Done<O<T>>): void {
+    if (!unit.__.includes('G')) {
+      done(undefined, 'not a graph')
+
+      return
+    }
+
+    done({
+      graph: unit as Graph,
+    })
+  }
+}
