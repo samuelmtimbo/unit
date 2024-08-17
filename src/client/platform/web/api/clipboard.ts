3d277f43c4b2575b156acbec868169f9bd0e4b21
Samuel MT <samuelmtimbo@gmail.com>
2024-08-17 18:47:59 -0300

    treat clipboard api not supported

--- src/client/platform/web/api/clipboard.ts
+++ src/client/platform/web/api/clipboard.ts
@@ -8,22 +8,28 @@ export function webClipboard(window: Window, opt: BootOpt): API['clipboard'] {
   const read = async () => {
     if (navigator.clipboard && navigator.clipboard.read) {
       const data = await navigator.clipboard.read()
+
       return data
     }
+
+    throw new APINotSupportedError('Clipboar Read')
   }
 
   const readText = async () => {
     if (navigator.clipboard && navigator.clipboard.readText) {
       const text = await navigator.clipboard.readText()
+      
       return text
     }
+
+    throw new APINotSupportedError('Clipboar Read Text')
   }
 
   const writeText = async (text: string) => {
     if (navigator.clipboard && navigator.clipboard.writeText) {
       await navigator.clipboard.writeText(text)
     } else {
-      throw new APINotSupportedError('Clipboard')
+      throw new APINotSupportedError('Clipboard Write Text')
     }
   }
 
