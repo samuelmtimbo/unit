30eaf9db08ab0ab5cacb3bba812c58950f339b38
Samuel MT <samuelmtimbo@gmail.com>
2024-09-20 00:42:35 -0300

    `worker` start url to use origin instead of href

--- src/client/platform/web/api/worker.ts
+++ src/client/platform/web/api/worker.ts
@@ -4,8 +4,8 @@ import { BootOpt } from '../../../../system'
 export function webWorker(window: Window, opt: BootOpt): API['worker'] {
   const worker = {
     start() {
-      const { href } = location
-      const url = `${href}_worker.js`
+      const { origin } = location
+      const url = `${origin}_worker.js`
       const worker = new Worker(url)
       return worker
     },
