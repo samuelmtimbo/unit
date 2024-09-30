026e365f6bc540c1061ead268056090459c730c5
Samuel MT <sam@ioun.it>
2024-09-30 12:50:39 -0700

    remove bad require lib

--- src/util/svg/index.ts
+++ src/util/svg/index.ts
@@ -1,5 +1,3 @@
-import svgPathBoundingBox = require('svg-path-bounding-box')
-
 export function getPathBoundingBox(d: string): {
   x1: number
   y1: number
@@ -12,7 +10,18 @@ export function getPathBoundingBox(d: string): {
   width: number
   height: number
 } {
-  const bb = svgPathBoundingBox(d)
+  const bb = {
+    x1: 0,
+    y1: 0,
+    x2: 0,
+    y2: 0,
+    minX: 0,
+    minY: 0,
+    maxX: 0,
+    maxY: 0,
+    width: 0,
+    height: 0,
+  }
 
   return bb
 }
