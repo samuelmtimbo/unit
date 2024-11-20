860477d7f908092eb5e4290ff6a77efd8b473f0f
Samuel MT <samuelmtimbo@gmail.com>
2024-11-20 01:28:20 -0300

    string `match all`

---
+++ src/system/f/string/MatchAll/index.ts
@@ -0,0 +1,40 @@
+import { Functional } from '../../../../Class/Functional'
+import { System } from '../../../../system'
+import { ID_MATCH_ALL } from '../../../_ids'
+
+export interface I {
+  str: string
+  regex: string
+}
+
+export interface O {
+  matches: string[][]
+}
+
+export default class MatchAll extends Functional<I, O> {
+  constructor(system: System) {
+    super(
+      {
+        i: ['str', 'regex'],
+        o: ['matches'],
+      },
+      {},
+      system,
+      ID_MATCH_ALL
+    )
+  }
+
+  f({ str, regex }: I, done): void {
+    const result: IterableIterator<RegExpExecArray> = str.matchAll(
+      new RegExp(regex, 'g')
+    )
+
+    const matches = []
+
+    for (const entry of result) {
+      matches.push(entry.map((v) => v ?? ''))
+    }
+
+    done({ matches })
+  }
+}
