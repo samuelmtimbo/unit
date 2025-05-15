a81a8222dae42694831e9189e870f43a287915d9
Samuel MT <samuelmtimbo@gmail.com>
2025-05-15 17:51:25 -0300

    `debug` unit

---
+++ src/system/f/unit/Debug/index.ts
@@ -0,0 +1,54 @@
+import { Done } from '../../../../Class/Functional/Done'
+import { Holder } from '../../../../Class/Holder'
+import { Moment } from '../../../../debug/Moment'
+import { System } from '../../../../system'
+import { $U } from '../../../../types/interface/async/$U'
+import { Async } from '../../../../types/interface/async/Async'
+import { ID_DEBUG } from '../../../_ids'
+
+export interface I<T> {
+  unit: $U
+  opt: {
+    events: string[]
+  }
+}
+
+export interface O<T> {
+  moment: Moment
+}
+
+export default class Debug<T> extends Holder<I<T>, O<T>> {
+  constructor(system: System) {
+    super(
+      {
+        fi: ['unit', 'opt'],
+        fo: [],
+        i: [],
+        o: ['moment'],
+      },
+      {
+        input: {
+          unit: {
+            ref: true,
+          },
+        },
+      },
+      system,
+      ID_DEBUG
+    )
+  }
+
+  f({ unit, opt }: I<T>, done: Done<O<T>>): void {
+    unit = Async(unit, ['U'], this.__system.async)
+
+    try {
+      unit.$watch(opt, (moment) => {
+        this._output.moment.push(moment)
+      })
+    } catch (err) {
+      done(undefined, err.message)
+
+      return
+    }
+  }
+}
