f73bceea34629d8ced89dfa91e141d3f227bc26c
Samuel MT <samuelmtimbo@gmail.com>
2025-05-30 18:42:06 -0300

    `article`, `footer`, `nav` and `section`

---
+++ src/system/platform/component/Article/Component.ts
@@ -0,0 +1,21 @@
+import HTMLElement_ from '../../../../client/html'
+import { System } from '../../../../system'
+import { Dict } from '../../../../types/Dict'
+
+export interface Props {
+  style?: Dict<string>
+  attr?: Dict<string>
+}
+
+export default class Article extends HTMLElement_<HTMLElement, Props> {
+  constructor($props: Props, $system: System) {
+    super(
+      $props,
+      $system,
+      $system.api.document.createElement('article'),
+      $system.style['article'],
+      {},
+      {}
+    )
+  }
+}
---
+++ src/system/platform/component/Nav/Component.ts
@@ -0,0 +1,21 @@
+import HTMLElement_ from '../../../../client/html'
+import { System } from '../../../../system'
+import { Dict } from '../../../../types/Dict'
+
+export interface Props {
+  style?: Dict<string>
+  attr?: Dict<string>
+}
+
+export default class Nav extends HTMLElement_<HTMLElement, Props> {
+  constructor($props: Props, $system: System) {
+    super(
+      $props,
+      $system,
+      $system.api.document.createElement('nav'),
+      $system.style['nav'],
+      {},
+      {}
+    )
+  }
+}
