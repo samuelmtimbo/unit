74a56ecc448f3bf2eae5712680071429480ce732
Samuel MT <samuelmtimbo@gmail.com>
2025-06-27 23:53:17 -0300

    Revert "fix fork debug moment to use given spec"
    This reverts commit 9a52a090c4558be04441ed51052e310e3c71fd69.

--- src/system/platform/component/app/Editor/Component.ts
+++ src/system/platform/component/app/Editor/Component.ts
@@ -56465,7 +56465,7 @@ export class Editor_ extends Element<HTMLDivElement, Props_> {
         return
       }
 
-      this._spec_fork_unit(unitId, forked_unit_spec.id, forked_unit_spec)
+      this._spec_fork_unit(unitId, forked_unit_spec.id)
       this._sim_fork_unit(unitId, forked_unit_spec.id)
     } else {
       const spec = this._get_unit_spec(unitId) as GraphSpec
