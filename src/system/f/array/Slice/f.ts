c4784c1aa7c70f1e6b9055169e4986fe46fe7fb4
Samuel MT <samuelmtimbo@gmail.com>
2025-08-30 02:21:30 -0300

    fix array `slice` 0 end

--- src/system/f/array/Slice/f.ts
+++ src/system/f/array/Slice/f.ts
@@ -7,5 +7,5 @@ export default function slice<T>({
   begin: number
   end: number
 }): { a: T[] } {
-  return { a: a.slice(begin, end || undefined) }
+  return { a: a.slice(begin, end ?? undefined) }
 }
