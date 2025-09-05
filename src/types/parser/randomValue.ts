827ef5ffe55ff7cf09d3f4baef2a330d9094406d
Samuel MT <samuelmtimbo@gmail.com>
2025-09-05 08:20:27 -0300

    add more string literal examples to prevent infinite change loop

--- src/types/parser/randomValue.ts
+++ src/types/parser/randomValue.ts
@@ -23,7 +23,7 @@ export function mapTree(values: string[]): TreeNode[] {
 }
 
 const TREE_VALUE_NUMBER = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-const TREE_VALUE_STRING = [`''`]
+const TREE_VALUE_STRING = [`''`, `'unit'`, `'foo'`]
 const TREE_VALUE_BOOLEAN = ['true', 'false']
 const TREE_VALUE_OBJECT = ['{}']
 const TREE_VALUE_ARRAY = ['[]', '[0]', '[0,1,2]']
