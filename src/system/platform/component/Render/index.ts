feb8321a93b4ff1acfd456eee853b85c2a86bff1
Samuel MT <samuelmtimbo@gmail.com>
2025-09-20 14:50:25 -0300

    remove `render` "style" and "attr" inputs

--- src/system/platform/component/Render/index.ts
+++ src/system/platform/component/Render/index.ts
@@ -1,14 +1,11 @@
 import { $ } from '../../../../Class/$'
 import { Element_ } from '../../../../Class/Element'
 import { System } from '../../../../system'
-import { Dict } from '../../../../types/Dict'
 import { $U } from '../../../../types/interface/async/$U'
 import { ID_RENDER } from '../../../_ids'
 
 export interface I {
   component: $U & $
-  style: object
-  attr: Dict<string>
 }
 
 export interface O {}
@@ -17,7 +14,7 @@ export default class Render extends Element_<I, O> {
   constructor(system: System) {
     super(
       {
-        i: ['style', 'component', 'attr'],
+        i: ['component'],
         o: [],
       },
       {
