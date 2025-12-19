5332d907ebc883cdd57cdfb9b72591c325e02294
Samuel MT <samuelmtimbo@gmail.com>
2025-12-19 03:27:58 -0300

    fix add http server wrap missing interface

--- src/wrap/Server.ts
+++ src/wrap/Server.ts
@@ -7,6 +7,8 @@ import { Unlisten } from '../types/Unlisten'
 
 export function wrapHTTPServer(server: Server, system: System): $ & SE {
   return new (class Server extends $ implements SE {
+    __: string[] = ['SE']
+
     listen(port: number, handler: ServerHandler, servers: Dict<any>): Unlisten {
       return server.listen(port, handler, servers)
     }
