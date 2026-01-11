37b0994927a6702be0e974dea17656af46f79fbc
Samuel MT <samuelmtimbo@gmail.com>
2026-01-11 00:28:22 -0300

    web dist to be copied to public

--- src/script/build/web.ts
+++ src/script/build/web.ts
@@ -1,47 +1,36 @@
-import { copy } from 'fs-extra';
+import { copyFile, ensureDir } from 'fs-extra';
+import * as path from 'path';
+import { PATH_PUBLIC } from '../../path';
 import { build } from '../build';
+
+const build_ = async (source: string, target: string, minify: boolean) => {
+  const outfile =  `dist/${target}${minify ? '.min' : ''}.js`
+
+  await build({
+    minify: true,
+    sourcemap: true,
+    bundle: true,
+    format: "iife",
+    logLevel: 'warning',
+    entryPoints: [`src/client/platform/web/${source}.ts`],
+    outfile,
+    metafile: true,
+  })
+
+  
+  await ensureDir(path.join(PATH_PUBLIC, 'dist'))
+  
+  const publicPath = path.join(PATH_PUBLIC, outfile)
+  
+  await copyFile(outfile, publicPath)
+}
 
 void (async () => {
   await Promise.all([
-  build({
-    minify: true,
-    sourcemap: true,
-    bundle: true,
-    format: "iife",
-    logLevel: 'warning',
-    entryPoints: ['src/client/platform/web/web.ts'],
-    outfile: 'dist/index.min.js',
-    metafile: true,
-  }),
-  build({
-    minify: false,
-    sourcemap: true,
-    bundle: true,
-    format: "iife",
-    logLevel: 'warning',
-    entryPoints: ['src/client/platform/web/web.ts'],
-    outfile: 'dist/index.js',
-    metafile: true,
-  }),
-  build({
-    minify: true,
-    sourcemap: true,
-    bundle: true,
-    format: "iife",
-    logLevel: 'warning',
-    entryPoints: ['src/client/platform/web/global.ts'],
-    outfile: 'dist/global.min.js',
-    metafile: true,
-  }),
-  build({
-    minify: false,
-    sourcemap: true,
-    bundle: true,
-    format: "iife",
-    logLevel: 'warning',
-    entryPoints: ['src/client/platform/web/global.ts'],
-    outfile: 'dist/global.js',
-    metafile: true,
-  })])
+    build_('web', 'index', true),
+    build_('web', 'index', false),
+    build_('global', 'global', true),
+    build_('global', 'global', false),
+  ])
 })()
 
