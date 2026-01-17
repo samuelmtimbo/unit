b15c2ab2dfb7fafdfb93c14ba5d4cbe910d4186a
Samuel MT <samuelmtimbo@gmail.com>
2026-01-17 13:40:26 -0300

    generic ref output `get` should have __ interface input

--- src/system/platform/method/object/Get1/index.ts
+++ src/system/platform/method/object/Get1/index.ts
@@ -1,13 +1,16 @@
+import { $ } from '../../../../../Class/$'
 import { Done } from '../../../../../Class/Functional/Done'
 import { Fail } from '../../../../../Class/Functional/Fail'
 import { Holder } from '../../../../../Class/Holder'
 import { System } from '../../../../../system'
-import { J } from '../../../../../types/interface/J'
+import { $J } from '../../../../../types/interface/async/$J'
+import { Async } from '../../../../../types/interface/async/Async'
 import { ID_GET_1 } from '../../../../_ids'
 
 export interface I<T> {
-  obj: J
+  obj: $J & $
   name: string
+  __: string[]
   done: any
 }
 
@@ -20,7 +23,7 @@ export default class Get1<T> extends Holder<I<T>, O<T>> {
   constructor(system: System) {
     super(
       {
-        fi: ['obj', 'name'],
+        fi: ['obj', 'name', '__'],
         fo: ['value'],
         i: [],
         o: ['done'],
@@ -43,18 +46,24 @@ export default class Get1<T> extends Holder<I<T>, O<T>> {
     )
   }
 
-  async f({ obj, name }: I<T>, done: Done<O<T>>, fail: Fail) {
-    let value: any
+  async f({ obj, name, __ }: I<T>, done: Done<O<T>>, fail: Fail) {
+    obj = Async(obj, obj.__, this.__system.async)
 
     try {
-      value = await obj.get(name)
+      const value = await obj.$ref({ name, __ })
+
+      if (value === null) {
+        fail('key not found')
+
+        return
+      }
+
+      done({ value })
     } catch (err) {
       fail(err.message)
 
       return
     }
-
-    done({ value })
   }
 
   b() {
