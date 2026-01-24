83a680230742453c3bb233500698a370d534a556
Samuel MT <samuelmtimbo@gmail.com>
2026-01-24 20:22:51 -0300

    fix async `set` to stringify data before call

--- src/system/platform/method/object/Set/index.ts
+++ src/system/platform/method/object/Set/index.ts
@@ -1,6 +1,7 @@
 import { Functional } from '../../../../../Class/Functional'
 import { Done } from '../../../../../Class/Functional/Done'
 import { Fail } from '../../../../../Class/Functional/Fail'
+import { stringify } from '../../../../../spec/stringify'
 import { System } from '../../../../../system'
 import { $J } from '../../../../../types/interface/async/$J'
 import { Async } from '../../../../../types/interface/async/Async'
@@ -38,7 +39,9 @@ export default class Set<T> extends Functional<I<T>, O<T>> {
   async f({ obj, name, data }: I<T>, done: Done<O<T>>, fail: Fail) {
     obj = Async(obj, ['J'], this.__system.async)
 
-    obj.$set({ name, data }, (_, err) => {
+    const _data = stringify(data)
+
+    obj.$set({ name, data: _data }, (_, err) => {
       fail(err)
 
       return
