5c84e1f745d8d2a4e3f9bfc2d89d8430c4ac60e3
Samuel MT <samuelmtimbo@gmail.com>
2026-01-25 22:14:30 -0300

    implement missing async object $J interface methods

--- src/types/interface/async/$J.ts
+++ src/types/interface/async/$J.ts
@@ -1,19 +1,40 @@
+import { ObjectUpdateType } from '../../../ObjectUpdateType'
 import { Callback } from '../../Callback'
 
-export const J_METHOD_GET = ['get']
-export const J_METHOD_CALL = ['set']
-export const J_METHOD_WATCH = []
+export const J_METHOD_GET = ['get', 'deepGet', 'hasKey', 'keys', 'deepHas']
+export const J_METHOD_CALL = ['set', 'delete', 'deepSet', 'deepDelete']
+export const J_METHOD_WATCH = ['subscribe']
 export const J_METHOD_REF = ['ref']
 
 export interface $J_G {
   $get(data: { name: string }, callback: Callback<any>): void
+  $deepGet(data: { path: string[] }, callback: Callback<any>): void
+  $hasKey(data: { name: string }, callback: Callback<boolean>): void
+  $keys(data: {}, callback: Callback<string[]>): void
+  $deepHas(data: { path: string[] }, callback: Callback<boolean>): void
 }
 
 export interface $J_C {
   $set(data: { name: string; data: any }, callback: Callback<any>): void
+  $delete(data: { name: string }, callback: Callback<any>): void
+  $deepSet(data: { path: string[]; data: any }, callback: Callback<any>): void
+  $deepDelete(
+    data: { name: string; path: string[] },
+    callback: Callback<any>
+  ): void
 }
 
-export interface $J_W {}
+export interface $J_W {
+  $subscribe(
+    data: { path: string[]; key: string },
+    callback: Callback<{
+      type: ObjectUpdateType
+      path: string[]
+      key: string
+      data: any
+    }>
+  ): void
+}
 
 export interface $J_R {
   $ref(data: { name: string; __: string[] }): any
--- src/types/interface/async/AsyncJ.ts
+++ src/types/interface/async/AsyncJ.ts
@@ -1,4 +1,5 @@
 import { $ } from '../../../Class/$'
+import { ObjectUpdateType } from '../../../ObjectUpdateType'
 import { evaluate } from '../../../spec/evaluate'
 import { Callback } from '../../Callback'
 import { J } from '../J'
@@ -6,7 +7,7 @@ import { $J, $J_C, $J_G, $J_R, $J_W } from './$J'
 import { Async } from './Async'
 
 export const AsyncJGet: (value: J) => $J_G = (value) => {
-  return {
+  const $get: $J_G = {
     $get({ name }: { name: string }, callback: Callback<any>): void {
       let data: any
 
@@ -14,14 +15,80 @@ export const AsyncJGet: (value: J) => $J_G = (value) => {
         data = value.get(name)
       } catch (err) {
         callback(undefined, err.message)
+
+        return
       }
+
+      callback(data)
+    },
+    $deepGet: function (
+      { path }: { path: string[] },
+      callback: Callback<any>
+    ): void {
+      let data: any
+
+      try {
+        data = value.deepGet(path)
+      } catch (err) {
+        callback(undefined, err.message)
+
+        return
+      }
+
+      callback(data)
+    },
+    $hasKey: function (
+      { name }: { name: string },
+      callback: Callback<boolean>
+    ): void {
+      let data: any
+
+      try {
+        data = value.hasKey(name)
+      } catch (err) {
+        callback(undefined, err.message)
+
+        return
+      }
+
+      callback(data)
+    },
+    $keys: function ({}: {}, callback: Callback<string[]>): void {
+      let data: any
+
+      try {
+        data = value.keys()
+      } catch (err) {
+        callback(undefined, err.message)
+
+        return
+      }
+
+      callback(data)
+    },
+    $deepHas: function (
+      { path }: { path: string[] },
+      callback: Callback<boolean>
+    ): void {
+      let data: any
+
+      try {
+        data = value.deepHas(path)
+      } catch (err) {
+        callback(undefined, err.message)
+
+        return
+      }
+
       callback(data)
     },
   }
+
+  return $get
 }
 
 export const AsyncJCall: (value: J) => $J_C = (value) => {
-  return {
+  const $call: $J_C = {
     $set(
       { name, data }: { name: string; data: string },
       callback: Callback<any>
@@ -32,15 +99,88 @@ export const AsyncJCall: (value: J) => $J_C = (value) => {
         value.set(name, _data)
       } catch (err) {
         callback(undefined, err.message)
+
+        return
+      }
+
+      callback()
+    },
+    $delete: function (
+      { name }: { name: string },
+      callback: Callback<any>
+    ): void {
+      try {
+        value.delete(name)
+      } catch (err) {
+        callback(undefined, err.message)
+
+        return
+      }
+
+      callback()
+    },
+    $deepSet: function (
+      { path, data }: { name: string; path: string[]; data: any },
+      callback: Callback<any>
+    ): void {
+      try {
+        value.deepSet(path, data)
+      } catch (err) {
+        callback(undefined, err.message)
+
+        return
+      }
+
+      callback()
+    },
+    $deepDelete: function (
+      { path }: { name: string; path: string[] },
+      callback: Callback<any>
+    ): void {
+      try {
+        value.deepDelete(path)
+      } catch (err) {
+        callback(undefined, err.message)
+
+        return
       }
 
       callback()
     },
   }
+
+  return $call
 }
 
 export const AsyncJWatch: (value: J) => $J_W = (value) => {
-  return {}
+  const $watch: $J_W = {
+    $subscribe: function (
+      { key, path }: { path: string[]; key: string },
+      callback: Callback<{
+        type: ObjectUpdateType
+        path: string[]
+        key: string
+        data: any
+      }>
+    ): void {
+      try {
+        value.subscribe(path, key, (type, path, key, data) => {
+          callback({
+            type,
+            path,
+            key,
+            data,
+          })
+        })
+      } catch (err) {
+        callback(undefined, err.message)
+
+        return
+      }
+    },
+  }
+
+  return $watch
 }
 
 export const AsyncJRef: (value: J) => $J_R = (value) => {
