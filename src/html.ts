e40a506d1aa3135b253c43a1d833db3179b0aa56
Samuel MT <admin@samuel.mt>
2026-07-22 09:09:25 -0300

    Revert "place base html template above root"
    This reverts commit 381295750adcdf6a157c32e8fd972a3810cfe099.

--- src/html.ts
+++ src/html.ts
@@ -109,7 +109,6 @@ export async function html(opt: WebTemplate): Promise<string> {
     ${head}
   </head>
   <body>
-    ${baseHtml || ''}
     <div
       id="__SYSTEM__ROOT__"
       style="
@@ -120,6 +119,7 @@ export async function html(opt: WebTemplate): Promise<string> {
         overflow: auto;
       "
     >
+      ${baseHtml || ''}
     </div>${
       script
         ? `<script type="text/javascript" src="${pathname ? `${pathname}/` : ''}index.js"></script>`
